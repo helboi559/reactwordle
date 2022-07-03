18d0d7bc01d8564221a4c4d7c8801db8e65dd405
Juan Ramirez <rami8224.2007@gmail.com>
2022-07-03 14:53:05 -0500

    added yellow functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-//work on enter,backspace, and color changes
-
 import {useState,useEffect} from 'react';
 import './App.css';
 import {
@@ -8,6 +6,8 @@ import {
   createdWordleLetter,
   createEmptyGuessList,
   deepCopy,
+  checkIsValidGuess,
+  countGuessResult,
 } from "./appUtil"
 
 const keyboardList = [
@@ -29,6 +29,7 @@ function App() {
   const [wordleAnswer,setWordleAnswer] = useState(wordleAnswerToday)
   const [wordleGuessIndex,setWordleGuessIndex] = useState(0)
   const [wordleLetterIndex,setWordleLetterIndex] = useState(0)
+  const [gameMessage,setGameMessage] = useState('')
   
   const [gameState,setGameState] = useState(GAME_STATE_ENUM.playing)
   // console.log('answer outside func',wordleAnswer)
@@ -43,50 +44,59 @@ function App() {
 
    //keyhandler functions
   const handleKeyPress = ({key}) => {
-    console.log(key)
+    // console.log(key)
     if (key === "Enter") {
-      console.log('if enter', key)
-      // handleEnterKey();
+      // console.log('if enter', key)
+      handleEnterKey();
     }
     if (key === "Backspace") {
-      console.log("Backspace key handler")
+      // console.log("Backspace key handler")
       // console.log('if backspace', key)
-      // handleBackSpace()
+      handleBackSpace()
     }
     if (letters.includes(key.toUpperCase())) {
       handleKeyEvent(key.toUpperCase())
-      console.log('handle other keys', key)
+      // console.log('handle other keys', key)
     }
    
     
   }
  
   const handleBackSpace = () => {
-    console.log('handle backspace')
+    // console.log('handle backspace')
       if(wordleLetterIndex === 0) return; //do nothing once it reaches zero
-      const newGuessList = JSON.parse(JSON.stringify(wordleGuessList))
+      const newGuessList = deepCopy(wordleGuessList)
       newGuessList[wordleGuessIndex][wordleLetterIndex - 1] = ''
       setWordleLetterIndex(wordleLetterIndex - 1)
       setWordleGuessList(newGuessList)
     
   }
-  // const handleEnterKey = () => {
-  //   if (wordleLetterIndex !== 5) return; // stop until it gets to the last letter in the row   
+  const handleEnterKey = () => {
+    if (wordleLetterIndex !== 5) return; // stop until it gets to the last letter in the row   
     
-  //   const newGuessList = JSON.parse(JSON.stringify(wordleGuessList))
-  //   // console.log(newGuessList[wordleGuessIndex])
-  //   const guess = newGuessList[wordleGuessIndex].join('').toLowerCase()
-
-  //   if(checkIsValidGuess(guess)) {
-  //     checkAndUpdateGameState(guess)
-  //     setWordleGuessIndex(wordleGuessIndex + 1)
-  //     setWordleLetterIndex(0)
-  //   } else {
-  //     alert('Word Not Found')
-  //   }
+    const newGuessList = deepCopy(wordleGuessList)
+    
+    const guessWord = newGuessList[wordleGuessIndex]
+    // console.log('guessWord',guessWord)
+    //convert letters to word  compare (case-sensetive)
+    const guess = guessWord.map(({letter}) => letter).join('').toLowerCase('')
+    // console.log('guessWord',guess)
+    //if guess exists
+    if(checkIsValidGuess(guess)) {
+      checkAndUpdateGameState(guess)
+      //reassign word obj by colors
+      const calcGuessResult = countGuessResult(guessWord,wordleAnswer)
+      newGuessList[wordleGuessIndex] = calcGuessResult
+      setWordleGuessList(newGuessList)
+      setWordleGuessIndex(wordleGuessIndex + 1)
+      setWordleLetterIndex(0)
+    } else {
+      // set message to try again
+      setGameMessage('Word is not in list, try again!')
+    }
     
     
-  // }
+  }
   const handleKeyEvent = (letter) => {
     const newKey = createdWordleLetter(letter.toUpperCase(),null)
      //until it gets to the second last letter in row
@@ -102,52 +112,29 @@ function App() {
     setWordleLetterIndex(wordleLetterIndex + 1)
     
   }
-  // const checkAndUpdateGameState = (guess) => {
+
+  const checkAndUpdateGameState = (guess) => {
     
-  //   if (guess === wordleAnswer) {
-  //     //if won
-  //     setGameState(GAME_STATE_ENUM.won)
-  //     return;
-  //   }
-  //   if(wordleGuessIndex === 5) {
-  //     //if lost(at the end of the guesses)
-  //     // setGameState([...gameStateList][2])
-  //     setGameState(GAME_STATE_ENUM.lost)
-  //     //move to the next game
-  //     return;
-  //   } 
-  //   // setGameState([...gameStateList][0])
-  //   setGameState(GAME_STATE_ENUM.playing)
-  //   return 
+    if (guess === wordleAnswer) {
+      //if won
+      setGameState(GAME_STATE_ENUM.won)
+      return;
+    }
+    if(wordleGuessIndex === 5) {
+      //if lost(at the end of the guesses)
+      // setGameState([...gameStateList][2])
+      setGameState(GAME_STATE_ENUM.lost)
+      //move to the next game
+      return;
+    } 
+    // setGameState([...gameStateList][0])
+    setGameState(GAME_STATE_ENUM.playing)
+    return 
       
     
-  // }
-  // const checkIsValidGuess = (guess) => {
-  //   // console.log(wordleAnswer.includes(guess.toLowerCase()))
-  //   if (!answerList.includes(guess) && !wordList.includes(guess)) {
-  //     return false
-  //   }
-    
-  //   return true
-  // }
-  const changeColor = (guess) => {
-    //change classname to true/false  if guess idx and answer index are the same
-    //wordleguessidx, letteridx
-    let answerSplit = wordleAnswer.split('')
-    guess = guess.toLowerCase().split('')
-    // for(let i = 0 ; i < )
-    console.log('answer',answerSplit)
-    console.log('guess',guess)
   }
-  // const isCorrect = (guess) => {
-  //   let answerSplit = wordleAnswer.split('')
-  //   guess = guess.toLowerCase().split('')
-  //   // console.log('answer',answerSplit)
-  //   // console.log('guess',guess)
-  //   answerSplit.forEach((letter,index) => {
-  //     console.log(letter)
-  //   })
-  // }
+ 
+ 
   return (
     <div className="App" >
       <header  className='App-header' >
@@ -155,6 +142,7 @@ function App() {
         <WordleHeader />
         <h1>Answer: {wordleAnswer}</h1>  
         <h2>GameState: {gameState}</h2>
+        <h4>Game Message: {gameMessage}</h4>
         <WordleGrid wordleGuessList={wordleGuessList} />
         <WordleKeyboard keyboardList={keyboardList} handleKeyEvent={handleKeyEvent} />
       </header>
@@ -171,7 +159,7 @@ const WordleHeader = () => {
 
 const WordleGridLetter = (props) => {
   return (
-     <div className={`wordle-grid-char wordle-grid-char-${props.wordleLetter}`} >
+     <div className={`wordle-grid-char wordle-grid-char-${props.wordleLetter.color? props.wordleLetter.color : 'none'}`} >
       {props.wordleLetter.letter}
     </div>
   )
@@ -237,49 +225,7 @@ const KeyboardLeter = (props) => {
 }
 
 
-// const letters = [
-//     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"
-//   ]
 
-//   useEffect(() => {
-//     window.addEventListener("keyup", handleKeyPress);
-//     return () => {
-//       window.removeEventListener("keyup", handleKeyPress);
-//     };
-//   }, [wordleLetterIndex, wordleGuessIndex]);
-
-//   const handleKeyPress = ({key}) => {
-//     if (key === "Enter") {
-//       console.log("Enter key handler")
-//     }
-//     if (key === "Backspace") {
-//       console.log("Backspace key handler")
-//     }
-//     if (letters.includes(key.toUpperCase())) {
-//       handleKeyEvent(key.toUpperCase())
-//     }
-//   }
-
-//   const handleKeyEvent = (newLetter) => {
-//     console.log("handleKeyEvent ", newLetter)
-//   }
-//useEffect - runs once when page loads and whenever you want, mostly used for API(fetch)
-// function useKeyPress(targetKey, handler) {
-//   const upHandler = ({ key }) => {
-//     console.log(key)
-//     if (key === targetKey) {
-//       handler(key)
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("keyup", upHandler);
-//     return () => {
-//       window.removeEventListener("keyup", upHandler);
-//     };
-//     }, []); 
-// }
-// console.log(answerList)
 
 
 export default App;
--- src/appUtil.js
+++ src/appUtil.js
@@ -18,12 +18,7 @@ export const createEmptyGuessList = (numLetters,numGuesses) => {
     return newList;
 }
 
-// export const dayIncrementor = () => {
-//   const startDate = new Date("6-10-2022");
-//   const today = new Date();
-//   const days = Math.floor((today - startDate) / (1000 * 60 * 60 * 24));
-//   return days;
-// };
+
 
 export const pickWordleAnswer = () => {
   const randomWord = answerList[Math.floor(Math.random() * answerList.length)]
@@ -36,6 +31,58 @@ export const GAME_STATE_ENUM = {
   lost:'LOST'
 }
 
+export const checkIsValidGuess = (guess) => {
+    // console.log(wordleAnswer.includes(guess.toLowerCase()))
+    if (!answerList.includes(guess) && !wordList.includes(guess)) {
+      return false
+    }
+    return true;
+}
+
 export const deepCopy = (info) => {
     return JSON.parse(JSON.stringify(info))
 }
+
+//count grey instances and change color
+export const countGuessResult = (guessWordArr,wordleAnswer) => {
+    //make counters for each letter to determine color
+    const answerLetterCount = {}
+    const guessLetterCount = {}
+    
+    const guessWordArrCopy = deepCopy(guessWordArr)
+    
+    //change all colors to grey in guess && keep letter count
+    for(let i = 0 ; i < guessWordArr.length ; i++) {
+        guessWordArrCopy[i] = {...guessWordArrCopy[i],color:'grey'}
+        const answerLetterCopy = wordleAnswer[i].toLowerCase()
+        if (answerLetterCount[answerLetterCopy] === undefined) {
+            answerLetterCount[answerLetterCopy] = 1
+        } else {
+            answerLetterCount[answerLetterCopy] ++
+        }
+    }
+    // console.log("answerLetterCount",answerLetterCount)
+    //green and yellow check
+     for(let i = 0 ; i < guessWordArr.length ; i++) {
+         const guessLetterCopy = guessWordArr[i].letter.toLowerCase()
+         console.log(wordleAnswer[i] , guessLetterCopy)
+         if (guessLetterCount[guessLetterCopy] === undefined) {
+             guessLetterCount[guessLetterCopy] = 1
+         }
+         if (wordleAnswer[i] === guessLetterCopy) {
+            guessWordArrCopy[i]= {...guessWordArrCopy[i], color:'green'}
+            guessLetterCount[guessLetterCopy] --;
+         } 
+         if (answerLetterCount[guessLetterCopy] !== undefined && guessLetterCount[guessLetterCopy] > 0) {
+             guessWordArrCopy[i] = {
+                 ...guessWordArrCopy[i],
+                 color:'yellow'
+             }
+             guessLetterCount[guessLetterCopy]--
+         }
+       
+     }
+     console.log("guessLetterCount",guessLetterCount)
+    return guessWordArrCopy;
+}
+
